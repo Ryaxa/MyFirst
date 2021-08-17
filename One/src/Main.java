c5cb73d83f4becaddd712595f76b3bd1b5368afd
Scouser96 <vital199617@gmail.com>
2021-08-17 20:42:43 +0300

    "Hello"

--- One/src/Main.java
+++ One/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         First first = new First();
-        System.out.println("Hello World");
+        System.out.println("Hello World jjnjn");
     }
 }
